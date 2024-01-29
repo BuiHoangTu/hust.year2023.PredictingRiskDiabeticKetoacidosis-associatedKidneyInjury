205cebc7b60b7948a384227bab80baf71156f6e8
bhtu <tubh200547@sis.hust.edu.vn>
2024-01-29 22:07:21 +0700

    mesure: bun

--- extract_mesurements.py
+++ extract_mesurements.py
@@ -9,14 +9,17 @@ TEMP_PATH = Path("tmp")
 TEMP_PATH.mkdir(parents=True, exist_ok=True)
 
 # other important mesurements 
-IMPORTANT_MESUREMENTS = {
-    51006: "bun",
+IMPORTANT_MESUREMENTS_ICU = {
     227519: "urine_output",
     224639: "weight",
     227457: "plt"
 }
 
-def extract_chartevents_mesurement(mesureId: int, outputFile: str):
+IMPORTANT_MESUREMENTS_LABEVENT = {
+    51006: "bun"    
+}
+
+def extract_chartevents_mesurement_from_icu(mesureId: int, outputFile: str):
     mesureChunks = []
 
     targetPatients = set(pd.read_csv(TEMP_PATH / "icu_target.csv", usecols=["stay_id"])["stay_id"])
@@ -33,6 +36,29 @@ def extract_chartevents_mesurement(mesureId: int, outputFile: str):
     dfMesure.to_csv(TEMP_PATH / outputFile)
     pass
 
+
+def extract_chartevents_mesurement_from_labevent(mesureId: int, outputFile: str):
+    mesureChunks = []
+
+    targetPatients = set(pd.read_csv(TEMP_PATH / "icu_target.csv", usecols=["hadm_id"])["hadm_id"])
+
+    for chunk in pd.read_csv(MIMIC_PATH / "hosp" / "labevents.csv", chunksize=10000):
+        isCreatinineRow = chunk["itemid"] == mesureId
+        isInTargetPatients = chunk["hadm_id"].isin(targetPatients)
+        
+        filteredChunk = chunk[isCreatinineRow & isInTargetPatients]
+        mesureChunks.append(filteredChunk)
+        pass
+    dfMesure = pd.concat(mesureChunks)
+    
+    dfMesure.to_csv(TEMP_PATH / outputFile)
+    pass
+
 if __name__ == "__main__":
-    for icdCode, icdName in IMPORTANT_MESUREMENTS.items():
-        extract_chartevents_mesurement(icdCode, "chartevent_" + icdName + ".csv")
+    # for icdCode, icdName in IMPORTANT_MESUREMENTS_ICU.items():
+    #     extract_chartevents_mesurement_from_icu(icdCode, "chartevent_" + icdName + ".csv")
+    #     pass
+    for icdCode, icdName in IMPORTANT_MESUREMENTS_LABEVENT.items():
+        extract_chartevents_mesurement_from_labevent(icdCode, "labevent_" + icdName + ".csv")
+        pass
+    pass
