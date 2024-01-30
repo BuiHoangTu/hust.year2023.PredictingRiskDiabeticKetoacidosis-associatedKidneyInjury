583794ea065ac61122234a78cd73907e7fa1f230
bhtu <tubh200547@sis.hust.edu.vn>
2024-01-30 15:17:23 +0700

    drop chunk_id, read numeric column of value instead

--- extract_mesurements.py
+++ extract_mesurements.py
@@ -26,6 +26,9 @@ def extract_chartevents_mesurement_from_icu(mesureId: int, outputFile: str):
     targetPatients = set(pd.read_csv(TEMP_PATH / "icu_target.csv", usecols=["stay_id"])["stay_id"])
 
     for chunk in pd.read_csv(MIMIC_PATH / "icu" / "chartevents.csv", chunksize=10000):
+        # remove chunk id 
+        chunk = chunk.iloc[:, 1:]
+        
         isCreatinineRow = chunk["itemid"] == mesureId
         isInTargetPatients = chunk["stay_id"].isin(targetPatients)
         
@@ -44,6 +47,9 @@ def extract_chartevents_mesurement_from_labevent(mesureId: int, outputFile: str)
     targetPatients = set(pd.read_csv(TEMP_PATH / "icu_target.csv", usecols=["hadm_id"])["hadm_id"])
 
     for chunk in pd.read_csv(MIMIC_PATH / "hosp" / "labevents.csv", chunksize=10000):
+        # remove chunk id 
+        chunk = chunk.iloc[:, 1:]
+        
         isCreatinineRow = chunk["itemid"] == mesureId
         isInTargetPatients = chunk["hadm_id"].isin(targetPatients)
         
