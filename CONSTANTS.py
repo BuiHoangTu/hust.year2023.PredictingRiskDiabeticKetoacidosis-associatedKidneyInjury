1994c5a8c748e6528d79e75b5e8cd32ac6f09954
bhtu <tubh200547@sis.hust.edu.vn>
2024-01-31 00:23:58 +0700

    adding mesures

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -7,3 +7,14 @@ MIMIC_PATH = Path("../mimiciv2.2/")
 # temporary path
 TEMP_PATH = Path("tmp")
 TEMP_PATH.mkdir(parents=True, exist_ok=True)
+
+IMPORTANT_MESUREMENTS_ICU = {
+    227519: "urine_output",
+    224639: "weight",
+    227457: "plt",
+    220615: "creatinine"
+}
+
+IMPORTANT_MESUREMENTS_LABEVENT = {
+    51006: "bun"    
+}
