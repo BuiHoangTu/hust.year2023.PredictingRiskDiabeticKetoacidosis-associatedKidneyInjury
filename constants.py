420da5d81c797896cc835f23cf769b70a98b8a31
bhtu <tubh200547@sis.hust.edu.vn>
2024-02-06 16:36:26 +0700

    adding icd constants

--- constants.py
+++ constants.py
@@ -20,3 +20,52 @@ PREPROCESSED_MESUREMENTS = ["sugar"]
 IMPORTANT_MESUREMENTS_LABEVENT = {
     51006: "bun"    
 }
+
+# Define ICD-9/10 codes for DKA
+DKA_CODE_V9 = [
+    "24910",
+    "24911",
+    "25010",
+    "25011",
+    "25012",
+    "25013",
+]
+DKA_CODE_V10 = [
+    "E81",
+    "E810",
+    "E811",
+    "E091",
+    "E0910",
+    "E0911",
+    "E101",
+    "E1010",
+    "E1011",
+    "E111",
+    "E1110",
+    "E1111",
+    "E131",
+    "E1310",
+    "E1311",
+]
+
+# Define CKD stage 5 codes
+CKD5_CODE_V9 = [
+    "40301",
+    "40311",
+    "40391",
+    "40402",
+    "40403",
+    "40412",
+    "40413",
+    "40492",
+    "40493",
+    "5855",
+    "5856",
+]
+CKD5_CODE_V10 = [
+    "N185",
+    "N186",  # stage 5 and End stage renal disease
+    "I120",
+    "I1311",
+    "I132",
+]
