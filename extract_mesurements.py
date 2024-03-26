bda299be9356c0d2059f2d9fa193609c5a4cbbdd
bhtu <tubh200547@sis.hust.edu.vn>
2024-03-26 22:18:16 +0700

    modify extract_mesurements.py to support multiple id at one

--- extract_mesurements.py
+++ extract_mesurements.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from constants import MIMIC_PATH, TEMP_PATH
 
-# other important mesurements 
+# other important mesurements
 IMPORTANT_MESUREMENTS_ICU = {
     227519: "urine_output",
     224639: "weight",
@@ -14,40 +14,56 @@ IMPORTANT_MESUREMENTS_LABEVENT = {
     51006: "bun"    
 }
 
-def extractChartEventMesures(itemId: int, outputFile: str|None) -> pd.DataFrame:
-    mesureChunks = []
-
-    targetPatients = set(pd.read_csv(TEMP_PATH / "target_patients.csv", usecols=["stay_id"])["stay_id"])
-
-    for chunk in pd.read_csv(MIMIC_PATH / "icu" / "chartevents.csv", chunksize=10000):
-        # remove chunk id 
-        chunk = chunk.iloc[:, 1:]
-        
-        isCreatinineRow = chunk["itemid"] == itemId
-        isInTargetPatients = chunk["stay_id"].isin(targetPatients)
-        
-        filteredChunk = chunk[isCreatinineRow & isInTargetPatients]
-        mesureChunks.append(filteredChunk)
-        pass
-    dfMesure = pd.concat(mesureChunks)
-    
-    if (outputFile):
-        dfMesure.to_csv(TEMP_PATH / outputFile)
-        pass
-    
-    return dfMesure
-
-
-def extractLabEventMesures(mesureId: int, outputFile: str|None) -> pd.DataFrame:
-    """Extract labevent of my target patients
+def extractChartEventMesures(itemId: int|list[int], outputFile: str|None) -> pd.DataFrame:
+    """Extract chartevent of my target patients
 
     Args:
-        mesureId (int): id of the mesure need extracting
+        mesureId (int|list[int]): id of the mesure(s) need extracting
         outputFile (str | None): File name to store after extract
 
     Returns:
         pd.DataFrame: mesure and its data
     """
+
+    if isinstance(itemId, int):
+        itemId = [itemId]
+    
+    mesureChunks = []
+
+    targetPatients = set(pd.read_csv(TEMP_PATH / "target_patients.csv", usecols=["stay_id"])["stay_id"])
+
+    for chunk in pd.read_csv(MIMIC_PATH / "icu" / "chartevents.csv", chunksize=10000):
+        # remove chunk id
+        chunk = chunk.iloc[:, 1:]
+
+        isCreatinineRow = chunk["itemid"] in itemId
+        isInTargetPatients = chunk["stay_id"].isin(targetPatients)
+
+        filteredChunk = chunk[isCreatinineRow & isInTargetPatients]
+        mesureChunks.append(filteredChunk)
+        pass
+    dfMesure = pd.concat(mesureChunks)
+
+    if (outputFile):
+        dfMesure.to_csv(TEMP_PATH / outputFile)
+        pass
+
+    return dfMesure
+
+
+def extractLabEventMesures(mesureId: int|list[int], outputFile: str|None) -> pd.DataFrame:
+    """Extract labevent of my target patients
+
+    Args:
+        mesureId (int|list[int]): id of the mesure(s) need extracting
+        outputFile (str | None): File name to store after extract
+
+    Returns:
+        pd.DataFrame: mesure and its data
+    """
+    
+    if (isinstance(mesureId, int)):
+        mesureId = [mesureId]
     
     mesureChunks = []
 
@@ -57,7 +73,7 @@ def extractLabEventMesures(mesureId: int, outputFile: str|None) -> pd.DataFrame:
         # remove chunk id 
         chunk = chunk.iloc[:, 1:]
         
-        isCreatinineRow = chunk["itemid"] == mesureId
+        isCreatinineRow = chunk["itemid"] in mesureId
         isInTargetPatients = chunk["hadm_id"].isin(targetPatients)
         
         filteredChunk = chunk[isCreatinineRow & isInTargetPatients]
