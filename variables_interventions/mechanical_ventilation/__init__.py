4664693831dc1dd66c0ad24a6c30270eaf36e2d6
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-11 11:17:45 +0700

    limit time of mv

--- variables_interventions/mechanical_ventilation/__init__.py
+++ variables_interventions/mechanical_ventilation/__init__.py
@@ -1,13 +1,29 @@
-from variables_interventions.mechanical_ventilation.ventilation import extractVentilation
+import pandas as pd
+from constants import TARGET_PATIENT_FILE, TEMP_PATH
+from variables_interventions.mechanical_ventilation.ventilation import (
+    extractVentilation,
+)
 
 
 def extractMechVent():
-    df = extractVentilation()
-    df["mechanical_ventilation"] = df["ventilation_status"].isin(
+    dfMV = extractVentilation()
+    dfMV["mechanical_ventilation"] = dfMV["ventilation_status"].isin(
         [
             "Tracheostomy",
             "InvasiveVent",
         ]
     )
+    dfMV["starttime"] = pd.to_datetime(dfMV["starttime"])
+    dfMV["endtime"] = pd.to_datetime(dfMV["endtime"])
 
-    return df[["stay_id", "mechanical_ventilation"]]
+    dfTargetPatient = pd.read_csv(TEMP_PATH / TARGET_PATIENT_FILE)
+    dfTargetPatient = dfTargetPatient[["stay_id", "intime"]]
+    dfTargetPatient["intime"] = pd.to_datetime(dfTargetPatient["intime"])
+
+    dfMerged = pd.merge(dfMV, dfTargetPatient, "inner", "stay_id")
+    dfMerged = dfMerged[
+        (dfMerged["starttime"] > (dfMerged["intime"] - pd.Timedelta(hours=6)))
+        & (dfMerged["endtime"] < (dfMerged["intime"] + pd.Timedelta(hours=24)))
+    ]
+
+    return dfMerged[["stay_id", "mechanical_ventilation"]]
