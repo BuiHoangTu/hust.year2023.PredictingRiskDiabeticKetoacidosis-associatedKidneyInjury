e319d98175d9b10929fd91c1413fd1b7cc2049fe
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-11 13:06:18 +0700

    create function to reduce mesure within first day

--- variables_interventions/mechanical_ventilation/__init__.py
+++ variables_interventions/mechanical_ventilation/__init__.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from constants import TARGET_PATIENT_FILE, TEMP_PATH
+from reduce_mesurements import reduceByCharttime
 from variables_interventions.mechanical_ventilation.ventilation import (
     extractVentilation,
 )
@@ -13,18 +12,7 @@ def extractMechVent():
             "InvasiveVent",
         ]
     )
-    dfMV["starttime"] = pd.to_datetime(dfMV["starttime"])
-    dfMV["endtime"] = pd.to_datetime(dfMV["endtime"])
-
-    dfTargetPatient = pd.read_csv(TEMP_PATH / TARGET_PATIENT_FILE)
-    dfTargetPatient = dfTargetPatient[["stay_id", "intime"]]
-    dfTargetPatient["intime"] = pd.to_datetime(dfTargetPatient["intime"])
-
-    dfMerged = pd.merge(dfMV, dfTargetPatient, "inner", "stay_id")
-    dfMerged = dfMerged[
-        (dfMerged["starttime"] > (dfMerged["intime"] - pd.Timedelta(hours=6)))
-        & (dfMerged["endtime"] < (dfMerged["intime"] + pd.Timedelta(hours=24)))
-    ]
+    dfMerged = reduceByCharttime(dfMV, "starttime", "endtime")
 
     dfMerged = dfMerged[["stay_id", "mechanical_ventilation"]]
     dfMerged = dfMerged[dfMerged["mechanical_ventilation"]]
--- variables_interventions/use_crrt.py
+++ variables_interventions/use_crrt.py
@@ -1,23 +1,12 @@
-import pandas as pd
 from akd_stage.crrt import extractCrrt
-from constants import TARGET_PATIENT_FILE, TEMP_PATH
+from reduce_mesurements import reduceByCharttime
 
 
 def extractUseCrrt():
     dfCrrt = extractCrrt()
-
     dfCrrt = dfCrrt.drop_duplicates("stay_id")
-
     dfCrrt["use_crrt"] = True
 
-    dfTargetPatient = pd.read_csv(TEMP_PATH / TARGET_PATIENT_FILE)
-    dfTargetPatient = dfTargetPatient[["stay_id", "intime"]]
-    dfTargetPatient["intime"] = pd.to_datetime(dfTargetPatient["intime"])
-
-    dfMerged = pd.merge(dfCrrt, dfTargetPatient, "inner", "stay_id")
-    dfMerged = dfMerged[
-        (dfMerged["charttime"] > (dfMerged["intime"] - pd.Timedelta(hours=6)))
-        & (dfMerged["charttime"] < (dfMerged["intime"] + pd.Timedelta(hours=24)))
-    ]
+    dfMerged = reduceByCharttime(dfCrrt)
 
     return dfMerged[["stay_id", "use_crrt"]]
