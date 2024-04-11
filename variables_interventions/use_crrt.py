88fb15ef8ce84b1959cb1a968adc8fdd8a29a6fd
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-11 11:30:57 +0700

    limit use_crrt time

--- variables_interventions/use_crrt.py
+++ variables_interventions/use_crrt.py
@@ -1,11 +1,23 @@
+import pandas as pd
 from akd_stage.crrt import extractCrrt
+from constants import TARGET_PATIENT_FILE, TEMP_PATH
 
 
 def extractUseCrrt():
     dfCrrt = extractCrrt()
 
-    dfCrrt = dfCrrt[["stay_id"]].drop_duplicates("stay_id")
+    dfCrrt = dfCrrt.drop_duplicates("stay_id")
 
     dfCrrt["use_crrt"] = True
 
-    return dfCrrt
+    dfTargetPatient = pd.read_csv(TEMP_PATH / TARGET_PATIENT_FILE)
+    dfTargetPatient = dfTargetPatient[["stay_id", "intime"]]
+    dfTargetPatient["intime"] = pd.to_datetime(dfTargetPatient["intime"])
+
+    dfMerged = pd.merge(dfCrrt, dfTargetPatient, "inner", "stay_id")
+    dfMerged = dfMerged[
+        (dfMerged["charttime"] > (dfMerged["intime"] - pd.Timedelta(hours=6)))
+        & (dfMerged["charttime"] < (dfMerged["intime"] + pd.Timedelta(hours=24)))
+    ]
+
+    return dfMerged[["stay_id", "use_crrt"]]
