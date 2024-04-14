783bebe370c243fe3e15e0151c29daf378c99ff1
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-14 22:56:36 +0700

    first_day_lab, get only first mesures

--- middle_query/first_day_lab.sql
+++ middle_query/first_day_lab.sql
@@ -1,25 +1,17 @@
 WITH cbc AS (
   SELECT ie.stay_id,
-    MIN(hematocrit) AS hematocrit_min,
-    MAX(hematocrit) AS hematocrit_max,
     FIRST_VALUE(hematocrit) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
     ) AS hematocrit_first,
-    MIN(hemoglobin) AS hemoglobin_min,
-    MAX(hemoglobin) AS hemoglobin_max,
     FIRST_VALUE(hemoglobin) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
     ) AS hemoglobin_first,
-    MIN(platelet) AS platelets_min,
-    MAX(platelet) AS platelets_max,
     FIRST_VALUE(platelet) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
     ) AS platelets_first,
-    MIN(wbc) AS wbc_min,
-    MAX(wbc) AS wbc_max,
     FIRST_VALUE(wbc) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
@@ -32,78 +24,54 @@ WITH cbc AS (
 ),
 chem AS (
   SELECT ie.stay_id,
-    MIN(albumin) AS albumin_min,
-    MAX(albumin) AS albumin_max,
-    FIRST_VALUE(wbc) OVER (
+    FIRST_VALUE(albumin) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(globulin) AS globulin_min,
-    MAX(globulin) AS globulin_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS albumin_first,
+    FIRST_VALUE(globulin) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(total_protein) AS total_protein_min,
-    MAX(total_protein) AS total_protein_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS globulin_first,
+    FIRST_VALUE(total_protein) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(aniongap) AS aniongap_min,
-    MAX(aniongap) AS aniongap_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS total_protein_first,
+    FIRST_VALUE(aniongap) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bicarbonate) AS bicarbonate_min,
-    MAX(bicarbonate) AS bicarbonate_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS aniongap_first,
+    FIRST_VALUE(bicarbonate) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bun) AS bun_min,
-    MAX(bun) AS bun_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bicarbonate_first,
+    FIRST_VALUE(bun) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(calcium) AS calcium_min,
-    MAX(calcium) AS calcium_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bun_first,
+    FIRST_VALUE(calcium) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(chloride) AS chloride_min,
-    MAX(chloride) AS chloride_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS calcium_first,
+    FIRST_VALUE(chloride) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(creatinine) AS creatinine_min,
-    MAX(creatinine) AS creatinine_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS chloride_first,
+    FIRST_VALUE(creatinine) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(glucose) AS glucose_min,
-    MAX(glucose) AS glucose_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS creatinine_first,
+    FIRST_VALUE(glucose) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(sodium) AS sodium_min,
-    MAX(sodium) AS sodium_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS glucose_first,
+    FIRST_VALUE(sodium) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(potassium) AS potassium_min,
-    MAX(potassium) AS potassium_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS sodium_first,
+    FIRST_VALUE(potassium) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first
+    ) AS potassium_first
   FROM icustays AS ie
     LEFT JOIN chemistry AS le ON le.subject_id = ie.subject_id
     AND le.charttime >= ie.intime - INTERVAL '6 HOUR'
@@ -112,66 +80,46 @@ chem AS (
 ),
 diff AS (
   SELECT ie.stay_id,
-    MIN(basophils_abs) AS abs_basophils_min,
-    MAX(basophils_abs) AS abs_basophils_max,
-    FIRST_VALUE(wbc) OVER (
+    FIRST_VALUE(basophils_abs) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(eosinophils_abs) AS abs_eosinophils_min,
-    MAX(eosinophils_abs) AS abs_eosinophils_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS abs_basophils_first,
+    FIRST_VALUE(eosinophils_abs) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(lymphocytes_abs) AS abs_lymphocytes_min,
-    MAX(lymphocytes_abs) AS abs_lymphocytes_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS abs_eosinophils_first,
+    FIRST_VALUE(lymphocytes_abs) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(monocytes_abs) AS abs_monocytes_min,
-    MAX(monocytes_abs) AS abs_monocytes_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS abs_lymphocytes_first,
+    FIRST_VALUE(monocytes_abs) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(neutrophils_abs) AS abs_neutrophils_min,
-    MAX(neutrophils_abs) AS abs_neutrophils_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS abs_monocytes_first,
+    FIRST_VALUE(neutrophils_abs) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(atypical_lymphocytes) AS atyps_min,
-    MAX(atypical_lymphocytes) AS atyps_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS abs_neutrophils_first,
+    FIRST_VALUE(atypical_lymphocytes) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bands) AS bands_min,
-    MAX(bands) AS bands_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS atyps_first,
+    FIRST_VALUE(bands) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(immature_granulocytes) AS imm_granulocytes_min,
-    MAX(immature_granulocytes) AS imm_granulocytes_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bands_first,
+    FIRST_VALUE(immature_granulocytes) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(metamyelocytes) AS metas_min,
-    MAX(metamyelocytes) AS metas_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS imm_granulocytes_first,
+    FIRST_VALUE(metamyelocytes) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(nrbc) AS nrbc_min,
-    MAX(nrbc) AS nrbc_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS metas_first,
+    FIRST_VALUE(nrbc) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first
+    ) AS nrbc_first
   FROM icustays AS ie
     LEFT JOIN blood_differential AS le ON le.subject_id = ie.subject_id
     AND le.charttime >= ie.intime - INTERVAL '6 HOUR'
@@ -180,42 +128,30 @@ diff AS (
 ),
 coag AS (
   SELECT ie.stay_id,
-    MIN(d_dimer) AS d_dimer_min,
-    MAX(d_dimer) AS d_dimer_max,
-    FIRST_VALUE(wbc) OVER (
+    FIRST_VALUE(d_dimer) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(fibrinogen) AS fibrinogen_min,
-    MAX(fibrinogen) AS fibrinogen_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS d_dimer_first,
+    FIRST_VALUE(fibrinogen) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(thrombin) AS thrombin_min,
-    MAX(thrombin) AS thrombin_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS fibrinogen_first,
+    FIRST_VALUE(thrombin) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(inr) AS inr_min,
-    MAX(inr) AS inr_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS thrombin_first,
+    FIRST_VALUE(inr) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(pt) AS pt_min,
-    MAX(pt) AS pt_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS inr_first,
+    FIRST_VALUE(pt) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ptt) AS ptt_min,
-    MAX(ptt) AS ptt_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS pt_first,
+    FIRST_VALUE(ptt) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first
+    ) AS ptt_first
   FROM icustays AS ie
     LEFT JOIN coagulation AS le ON le.subject_id = ie.subject_id
     AND le.charttime >= ie.intime - INTERVAL '6 HOUR'
@@ -224,72 +160,50 @@ coag AS (
 ),
 enz AS (
   SELECT ie.stay_id,
-    MIN(alt) AS alt_min,
-    MAX(alt) AS alt_max,
-    FIRST_VALUE(wbc) OVER (
+    FIRST_VALUE(alt) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(alp) AS alp_min,
-    MAX(alp) AS alp_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS alt_first,
+    FIRST_VALUE(alp) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ast) AS ast_min,
-    MAX(ast) AS ast_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS alp_first,
+    FIRST_VALUE(ast) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(amylase) AS amylase_min,
-    MAX(amylase) AS amylase_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS ast_first,
+    FIRST_VALUE(amylase) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bilirubin_total) AS bilirubin_total_min,
-    MAX(bilirubin_total) AS bilirubin_total_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS amylase_first,
+    FIRST_VALUE(bilirubin_total) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bilirubin_direct) AS bilirubin_direct_min,
-    MAX(bilirubin_direct) AS bilirubin_direct_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bilirubin_total_first,
+    FIRST_VALUE(bilirubin_direct) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(bilirubin_indirect) AS bilirubin_indirect_min,
-    MAX(bilirubin_indirect) AS bilirubin_indirect_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bilirubin_direct_first,
+    FIRST_VALUE(bilirubin_indirect) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ck_cpk) AS ck_cpk_min,
-    MAX(ck_cpk) AS ck_cpk_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS bilirubin_indirect_first,
+    FIRST_VALUE(ck_cpk) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ck_mb) AS ck_mb_min,
-    MAX(ck_mb) AS ck_mb_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS ck_cpk_first,
+    FIRST_VALUE(ck_mb) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ggt) AS ggt_min,
-    MAX(ggt) AS ggt_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS ck_mb_first,
+    FIRST_VALUE(ggt) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first,
-    MIN(ld_ldh) AS ld_ldh_min,
-    MAX(ld_ldh) AS ld_ldh_max,
-    FIRST_VALUE(wbc) OVER (
+    ) AS ggt_first,
+    FIRST_VALUE(ld_ldh) OVER (
       PARTITION BY stay_id
       ORDER BY charttime
-    ) AS wbc_first
+    ) AS ld_ldh_first
   FROM icustays AS ie
     LEFT JOIN enzyme AS le ON le.subject_id = ie.subject_id
     AND le.charttime >= ie.intime - INTERVAL '6 HOUR'
@@ -299,142 +213,56 @@ enz AS (
 SELECT ie.subject_id,
   ie.stay_id,
   /* complete blood count */
-  hematocrit_min,
-  hematocrit_max,
-  hematocrit_first,
-  hemoglobin_min,
-  hemoglobin_max,
-  hemoglobin_first,
-  platelets_min,
-  platelets_max,
-  platelets_first,
-  wbc_min,
-  wbc_max,
-  wbc_first,
+  cbc.hematocrit_first,
+  cbc.hemoglobin_first,
+  cbc.platelets_first,
+  cbc.wbc_first,
   /* chemistry */
-  albumin_min,
-  albumin_max,
-  albumin_first,
-  globulin_min,
-  globulin_max,
-  globulin_first,
-  total_protein_min,
-  total_protein_max,
-  total_protein_first,
-  aniongap_min,
-  aniongap_max,
-  aniongap_first,
-  bicarbonate_min,
-  bicarbonate_max,
-  bicarbonate_first,
-  bun_min,
-  bun_max,
-  bun_first,
-  calcium_min,
-  calcium_max,
-  calcium_first,
-  chloride_min,
-  chloride_max,
-  chloride_first,
-  creatinine_min,
-  creatinine_max,
-  creatinine_first,
-  glucose_min,
-  glucose_max,
-  glucose_first,
-  sodium_min,
-  sodium_max,
-  sodium_first,
-  potassium_min,
-  potassium_max,
-  potassium_first,
+  chem.albumin_first,
+  chem.globulin_first,
+  chem.total_protein_first,
+  chem.aniongap_first,
+  chem.bicarbonate_first,
+  chem.bun_first,
+  chem.calcium_first,
+  chem.chloride_first,
+  chem.creatinine_first,
+  chem.glucose_first,
+  chem.sodium_first,
+  chem.potassium_first,
   /* blood differential */
-  abs_basophils_min,
-  abs_basophils_max,
-  abs_basophils_first,
-  abs_eosinophils_min,
-  abs_eosinophils_max,
-  abs_eosinophils_first,
-  abs_lymphocytes_min,
-  abs_lymphocytes_max,
-  abs_lymphocytes_first,
-  abs_monocytes_min,
-  abs_monocytes_max,
-  abs_monocytes_first,
-  abs_neutrophils_min,
-  abs_neutrophils_max,
-  abs_neutrophils_first,
-  atyps_min,
-  atyps_max,
-  atyps_first,
-  bands_min,
-  bands_max,
-  bands_first,
-  imm_granulocytes_min,
-  imm_granulocytes_max,
-  imm_granulocytes_first,
-  metas_min,
-  metas_max,
-  metas_first,
-  nrbc_min,
-  nrbc_max,
-  nrbc_first,
+  diff.abs_basophils_first,
+  diff.abs_eosinophils_first,
+  diff.abs_lymphocytes_first,
+  diff.abs_monocytes_first,
+  diff.abs_neutrophils_first,
+  diff.atyps_first,
+  diff.bands_first,
+  diff.imm_granulocytes_first,
+  diff.metas_first,
+  diff.nrbc_first,
   /* coagulation */
-  d_dimer_min,
-  d_dimer_max,
-  d_dimer_first,
-  fibrinogen_min,
-  fibrinogen_max,
-  fibrinogen_first,
-  thrombin_min,
-  thrombin_max,
-  thrombin_first,
-  inr_min,
-  inr_max,
-  inr_first,
-  pt_min,
-  pt_max,
-  pt_first,
-  ptt_min,
-  ptt_max,
-  ptt_first,
+  coag.d_dimer_first,
+  coag.fibrinogen_first,
+  coag.thrombin_first,
+  coag.inr_first,
+  coag.pt_first,
+  coag.ptt_first,
   /* enzymes and bilirubin */
-  alt_min,
-  alt_max,
-  alt_first,
-  alp_min,
-  alp_max,
-  alp_first,
-  ast_min,
-  ast_max,
-  ast_first,
-  amylase_min,
-  amylase_max,
-  amylase_first,
-  bilirubin_total_min,
-  bilirubin_total_max,
-  bilirubin_total_first,
-  bilirubin_direct_min,
-  bilirubin_direct_max,
-  bilirubin_direct_first,
-  bilirubin_indirect_min,
-  bilirubin_indirect_max,
-  bilirubin_indirect_first,
-  ck_cpk_min,
-  ck_cpk_max,
-  ck_cpk_first,
-  ck_mb_min,
-  ck_mb_max,
-  ck_mb_first,
-  ggt_min,
-  ggt_max,
-  ggt_first,
-  ld_ldh_min,
-  ld_ldh_max,
-  ld_ldh_first
+  enz.alt_first,
+  enz.alp_first,
+  enz.ast_first,
+  enz.amylase_first,
+  enz.bilirubin_total_first,
+  enz.bilirubin_direct_first,
+  enz.bilirubin_indirect_first,
+  enz.ck_cpk_first,
+  enz.ck_mb_first,
+  enz.ggt_first,
+  enz.ld_ldh_first
 FROM icustays AS ie
   LEFT JOIN cbc ON ie.stay_id = cbc.stay_id
   LEFT JOIN chem ON ie.stay_id = chem.stay_id
   LEFT JOIN diff ON ie.stay_id = diff.stay_id
   LEFT JOIN coag ON ie.stay_id = coag.stay_id
-  LEFT JOIN enz ON ie.stay_id = enz.stay_id
+  LEFT JOIN enz ON ie.stay_id = enz.stay_id;
