2419dd3d337a0a716de718b149d1843c4c691a11
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-15 20:36:23 +0700

    group first_day_lab because duplicated

--- variables_lab_test/first_day_lab.py
+++ variables_lab_test/first_day_lab.py
@@ -2,4 +2,6 @@ from middle_query import first_day_lab
 
 
 def extractFirstDayLab():
-    return first_day_lab.runSql()
+    df = first_day_lab.runSql()
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+    return df.groupby("stay_id").agg(lambda x: x.mean()).reset_index()
