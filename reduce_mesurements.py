a10b18c1e17fc1eb41a0c6767bdfebcb24ec2e2d
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-16 12:14:52 +0700

    add reduce df by hadm_id

--- reduce_mesurements.py
+++ reduce_mesurements.py
@@ -27,3 +27,19 @@ def reduceByStayId(df: pd.DataFrame, starttimeCol="charttime", endtimeCol="chart
     ]
 
     return dfMerged
+
+def reduceByHadmId(df: pd.DataFrame, starttimeCol="charttime", endtimeCol="charttime"):
+    df[starttimeCol] = pd.to_datetime(df[starttimeCol])
+    df[endtimeCol] = pd.to_datetime(df[endtimeCol])
+
+    dfTargetPatient = pd.read_csv(TEMP_PATH / TARGET_PATIENT_FILE)
+    dfTargetPatient = dfTargetPatient[["hadm_id", "stay_id", "intime"]]
+    dfTargetPatient["intime"] = pd.to_datetime(dfTargetPatient["intime"])
+
+    dfMerged = pd.merge(df, dfTargetPatient, "inner", "hadm_id")
+    dfMerged = dfMerged[
+        (dfMerged[starttimeCol] > (dfMerged["intime"] - pd.Timedelta(hours=6)))
+        & (dfMerged[endtimeCol] < (dfMerged["intime"] + pd.Timedelta(hours=24)))
+    ]
+
+    return dfMerged
