3a8747745a6c49ec7ec19834b19f8c3ec42350cc
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-18 13:17:58 +0700

    demographic add gender

--- variables_demographics/__init__.py
+++ variables_demographics/__init__.py
@@ -18,3 +18,17 @@ def getAge():
     dfMerged["age"] = pd.to_datetime(dfMerged["intime"]).dt.year - dfMerged["anchor_year"] + dfMerged["anchor_age"]
 
     return dfMerged[["stay_id", "age"]]
+
+def getGender():
+    """Get patients's biological gender
+
+    Returns:
+        pd.DataFrame: consists of stay_id, gender(M,F)
+    """
+    
+    dfPatientICU = getTargetPatientIcu() # intime 
+    dfPatient = pd.read_csv(MIMIC_PATH / "hosp" / "patients.csv")
+
+    dfMerged = pd.merge(dfPatientICU, dfPatient, "inner", on="subject_id")
+
+    return dfMerged[["stay_id", "gender"]]
