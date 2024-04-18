c7d7e6b12005bf171ae43cf4e8386fc07bb8a682
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-18 13:05:59 +0700

    demographic add age

---
+++ variables_demographics/__init__.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from constants import MIMIC_PATH
+from patients import getTargetPatientIcu
+
+
+def getAge():
+    """Get approximate age of patients according to mimic-iv
+
+    Returns:
+        pd.DataFrame: consists of stay_id, age
+    """
+     
+    # intime - anchor_year + anchor_age
+    dfPatientICU = getTargetPatientIcu() # intime 
+    dfPatient = pd.read_csv(MIMIC_PATH / "hosp" / "patients.csv")
+
+    dfMerged = pd.merge(dfPatientICU, dfPatient, "inner", on="subject_id")
+    dfMerged["age"] = pd.to_datetime(dfMerged["intime"]).dt.year - dfMerged["anchor_year"] + dfMerged["anchor_age"]
+
+    return dfMerged[["stay_id", "age"]]
