eafea81d4fcc0d954e6e82588ecf44a2dd279eba
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-20 00:13:57 +0700

    refactor gcs function and returned values

--- variables_scoring_systems/gcs.py
+++ variables_scoring_systems/gcs.py
@@ -1,5 +1,10 @@
 from middle_query import first_day_gcs
 
 
-def extractGsc():
-    return first_day_gcs.runSql()
+def get():
+    df = first_day_gcs.runSql()
+
+    df["gcs"] = df["gcs_min"]
+    df = df[["stay_id", "gcs"]]
+
+    return df
