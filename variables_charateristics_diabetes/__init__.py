6adcc15969b55f6ffa049e6f0dc1c8eedd3d921b
bhtu <tubh200547@sis.hust.edu.vn>
2024-04-20 11:48:03 +0700

    char_diabete add diabete type

--- variables_charateristics_diabetes/__init__.py
+++ variables_charateristics_diabetes/__init__.py
@@ -1,7 +1,18 @@
+import patients
 from variables_charateristics_diabetes import macroangiopathy, microangiopathy
+
+def getDiabeteType():
+    """Get patient diabete type: 1, 2, 0 - Others
+
+    Returns:
+        pandas.DataFrame: ["stay_id", "dka_type"]
+    """
+    df = patients.getNotebookOutput()
+    return df[["stay_id", "dka_type"]]
 
 def getMacroangiopathy():
     return macroangiopathy.get()
+
 
 def getMicroangiopathy():
     return microangiopathy.get()
