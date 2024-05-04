bd09dde7fdd677dbf019cf9666803b9a2ec1fd27
bhtu <tubh200547@sis.hust.edu.vn>
2024-05-04 11:09:11 +0700

    resolve for static variables in Patient

--- class_patient.py
+++ class_patient.py
@@ -2,29 +2,51 @@ from datetime import datetime
 import json
 from pathlib import Path
 from typing import Callable, Collection, Dict, List, Tuple
+import numpy as np
 from numpy import datetime64
 from pandas import DataFrame, Timestamp, to_datetime
 from sortedcontainers import SortedDict
 
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
 class Patient:
 
-    def __init__(self, subjectId: int, hadmId: int, stayId: int) -> None:
+    def __init__(self, subjectId: int, hadmId: int, stayId: int, akdPositive: bool) -> None:
         self.subjectId = subjectId
         self.hadmId = hadmId
         self.stayId = stayId
-        self.measures: Dict[str, Dict[Timestamp, float]] = SortedDict()
+        self.akdPositive = akdPositive
+        self.measures: Dict[str, Dict[Timestamp, float] | float] = SortedDict()
         pass
 
     def putMeasure(
         self,
         measureName: str,
-        measureTime: str | datetime | datetime64 | Timestamp,
+        measureTime: str | datetime | datetime64 | Timestamp | None,
         measureValue: float,
     ):
+
+        if measureTime is None:
+            self.measures[measureName] = measureValue
+            return 
+
         measureTime = to_datetime(measureTime)
 
         measure = self.measures.get(measureName)
+
+        if isinstance(measure, float) or isinstance(measure, int):
+            self.measures[measureName] = measureValue
+            return 
 
         if measure is None:
             measure = self.measures[measureName] = SortedDict()
@@ -71,7 +93,7 @@ class Patient:
         }
         if how in howMapping:
             how = howMapping[how]
-            
+
         if not isinstance(how, Callable): 
             raise Exception("Unk how: ", how)
 
@@ -85,35 +107,39 @@ class Patient:
 
         for measureName, measureTimeValue in self.measures.items():
 
-            measureTimes = list(measureTimeValue.keys())
-            left = 0
-            right = len(measureTimeValue) - 1
+            if isinstance(measureTimeValue, dict):
+                measureTimes = list(measureTimeValue.keys())
+                left = 0
+                right = len(measureTimeValue) - 1
 
-            while left <= right:
-                mid = left + (right - left) // 2
+                while left <= right:
+                    mid = left + (right - left) // 2
 
-                if measureTimes[mid] >= fromTime:
-                    startId = mid
-                    right = mid - 1
+                    if measureTimes[mid] >= fromTime:
+                        startId = mid
+                        right = mid - 1
 
-                else:
-                    left = mid + 1
+                    else:
+                        left = mid + 1
+                        pass
                     pass
+
+                measureInRange: List[Tuple[Timestamp, float]] = []
+                for i in range(startId, len(measureTimes)):
+                    if measureTimes[i] > toTime:
+                        break
+
+                    measureInRange.append((measureTimes[i], measureTimeValue[measureTimes[i]]))
+                    pass
+
+                dfMeasures = DataFrame(measureInRange, columns=["time", "value"])
+                measureValue = how(dfMeasures)
+
+                df[measureName] = measureValue
                 pass
-
-            measureInRange: List[Tuple[Timestamp, float]] = []
-            for i in range(startId, len(measureTimes)):
-                if measureTimes[i] > toTime:
-                    break
-
-                measureInRange.append((measureTimes[i], measureTimeValue[measureTimes[i]]))
+            else:
+                df[measureName] = measureTimeValue
                 pass
-
-            dfMeasures = DataFrame(measureInRange, columns=["time", "value"])
-            measureValue = how(dfMeasures)
-            
-            
-            df[measureName] = measureValue
             pass
 
         return df
@@ -123,12 +149,18 @@ class Patient:
             "subjectId": self.subjectId,
             "hadmId": self.hadmId,
             "stayId": self.stayId,
+            "akdPositive": self.akdPositive,
             "measures": {},
         }
         for measureName, measureData in self.measures.items():
-            jsonData["measures"][measureName] = {}
-            for timestamp, value in measureData.items():
-                jsonData["measures"][measureName][timestamp.isoformat()] = value
+            if isinstance(measureData, dict):
+                jsonData["measures"][measureName] = {}
+                for timestamp, value in measureData.items():
+                    jsonData["measures"][measureName][timestamp.isoformat()] = value
+                    pass
+                pass
+            else:
+                jsonData["measures"][measureName] = measureData         
         return jsonData
 
 
@@ -137,7 +169,7 @@ def toJsonFile(patients: Collection[Patient], file: str | Path):
     for obj in patients:
         jsonData.append(obj.toJson())
 
-    Path(file).write_text(json.dumps(jsonData, indent=4))
+    Path(file).write_text(json.dumps(jsonData, indent=4, cls=NpEncoder))
 
 
 def fromJsonFile(file: str | Path):
@@ -146,7 +178,9 @@ def fromJsonFile(file: str | Path):
     jsonData = json.loads(file.read_text())
     patients = []
     for item in jsonData:
-        patient = Patient(item["subjectId"], item["hadmId"], item["stayId"])
+        patient = Patient(
+            item["subjectId"], item["hadmId"], item["stayId"], item["akdPositive"]
+        )
         for measureName, measureData in item["measures"].items():
             for timestampStr, value in measureData.items():
                 timestamp = Timestamp(timestampStr)
