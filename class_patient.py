b06df48eaca68010bbf2d63661b45a5b3e6ee1d0
bhtu <tubh200547@sis.hust.edu.vn>
2024-05-05 22:26:11 +0700

    Patient: rename  to keep name unify with mimic's name; add intime; getMeasures use intime as pivot

--- class_patient.py
+++ class_patient.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Callable, Collection, Dict, List, Tuple
 import numpy as np
 from numpy import datetime64
+import pandas as pd
 from pandas import DataFrame, Timestamp, to_datetime
 from sortedcontainers import SortedDict
 from constants import TEMP_PATH
@@ -49,6 +50,8 @@ class PatientJsonEncoder(json.JSONEncoder):
             return float(obj)
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        if isinstance(obj, Timestamp):
+            return obj.isoformat()
         return super(PatientJsonEncoder, self).default(obj)
 
 
@@ -59,12 +62,14 @@ class Patient:
         subjectId: int,
         hadmId: int,
         stayId: int,
+        intime: str | datetime | datetime64 | Timestamp,
         akdPositive: bool,
         measures: Dict[str, Dict[Timestamp, float] | float] | None = None,
     ) -> None:
-        self.subjectId = subjectId
-        self.hadmId = hadmId
-        self.stayId = stayId
+        self.subject_id = subjectId
+        self.hadm_id = hadmId
+        self.stay_id = stayId
+        self.intime = to_datetime(intime)
         self.akdPositive = akdPositive
         self.measures: Dict[str, Dict[Timestamp, float] | float] = SortedDict()
 
@@ -108,15 +113,15 @@ class Patient:
 
     def getMeasuresBetween(
         self,
-        fromTime: str | datetime | datetime64 | Timestamp,
-        toTime: str | datetime | datetime64 | Timestamp,
+        fromTime: pd.Timedelta,
+        toTime: pd.Timedelta,
         how: str | Callable[[DataFrame], float] = "avg",
     ):
         """Get patient's status during specified period.
 
         Args:
-            fromTime (str | datetime | datetime64 | Timestamp): start time
-            toTime (str | datetime | datetime64 | Timestamp): end time
+            fromTime (pd.Timedelta): start time compare to intime (icu admission)
+            toTime (pd.Timedelta): end time compare to intime (icu admission)
             how : {'first', 'last', 'avg', 'max', 'min', 'std'} | Callable[[DataFrame], float], default 'avg'
                 Which value to choose if multiple exist:
 
@@ -133,8 +138,6 @@ class Patient:
         """
 
         # unify input
-        fromTime = to_datetime(fromTime)
-        toTime = to_datetime(toTime)
         howMapping: Dict[str, Callable[[DataFrame], float]] = {
             "first": lambda df: df.loc[df["time"].idxmin(), "value"],  # type: ignore
             "last": lambda df: df.loc[df["time"].idxmax(), "value"],
@@ -151,9 +154,9 @@ class Patient:
 
         df = DataFrame(
             {
-                "subject_id": self.subjectId,
-                "hadm_id": self.hadmId,
-                "stay_id": self.stayId,
+                "subject_id": self.subject_id,
+                "hadm_id": self.hadm_id,
+                "stay_id": self.stay_id,
             }
         )
 
@@ -167,7 +170,7 @@ class Patient:
                 while left <= right:
                     mid = left + (right - left) // 2
 
-                    if measureTimes[mid] >= fromTime:
+                    if measureTimes[mid] >= self.intime + fromTime:
                         startId = mid
                         right = mid - 1
 
@@ -178,7 +181,7 @@ class Patient:
 
                 measureInRange: List[Tuple[Timestamp, float]] = []
                 for i in range(startId, len(measureTimes)):
-                    if measureTimes[i] > toTime:
+                    if measureTimes[i] > self.intime + toTime:
                         break
 
                     measureInRange.append((measureTimes[i], measureTimeValue[measureTimes[i]]))
@@ -221,18 +224,17 @@ class Patients:
         if storagePath is None:
             storagePath = DEFAULT_PATIENTS_FILE
         storagePath = Path(storagePath)
-        
+
         self.storagePath = storagePath
-        
+
         if storagePath.exists():
             self.patientList = Patients.fromJsonFile(storagePath)
         else:
             patientList: List[Patient] = []
             self.patientList = patientList
-            
+
             dfPatient = getTargetPatientIcu()
             dfPatient = dfPatient[["subject_id", "hadm_id", "stay_id"]]
-
 
             dfAkd = akd_positive.extractKdigoStages7day()
             dfAkd["akd"] = dfAkd["aki_7day"]
@@ -242,224 +244,181 @@ class Patients:
             dfData1["akd"] = dfData1["akd"].astype(bool)
 
             for _, row in dfData1.iterrows():
-                patient = Patient(row["subject_id"], row["hadm_id"], row["stay_id"], row["akd"])
+                patient = Patient(
+                    row["subject_id"], 
+                    row["hadm_id"], 
+                    row["stay_id"], 
+                    row["intime"],
+                    row["akd"],
+                    )
                 patientList.append(patient)
                 pass
 
             dfData1["akd"].value_counts()
-
 
             ########### Characteristics of diabetes ###########
             df = getDiabeteType()
             df["dka_type"] = df["dka_type"].astype(int)
             self._putDataForPatients(df)
 
-
             df = getMacroangiopathy()
             self._putDataForPatients(df)
 
-
             df = getMicroangiopathy()
             self._putDataForPatients(df)
-
 
             ########### Demographics ###########
             df = getAge()
             self._putDataForPatients(df)
 
-
             df = getGender()
             self._putDataForPatients(df)
-
 
             df = getEthnicity()
             self._putDataForPatients(df)
 
-
             df = getHeight()
             self._putDataForPatients(df)
 
-
             df = getWeight()
             self._putDataForPatients(df)
-
 
             ########### Laboratory test ###########
             df = lab_test.getWbc().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getLymphocyte().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getHb().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getPlt().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getPO2().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getPCO2().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.get_pH().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getAG().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getBicarbonate().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getBun().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getCalcium().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getScr().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getBg().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getPhosphate().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getAlbumin().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.get_eGFR().dropna()
             self._putDataForPatients(df)
-
 
             df = lab_test.getHbA1C().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getCrp().dropna()
             self._putDataForPatients(df)
 
-
             df = lab_test.getUrineKetone().dropna()
             self._putDataForPatients(df)
-
 
             ########### Scoring systems ###########
             df = getGcs().dropna()
             self._putDataForPatients(df)
 
-
             df = getOasis().dropna()
             self._putDataForPatients(df)
-
 
             df = getSofa()
             self._putDataForPatients(df)
 
-
             df = getSaps2()
             self._putDataForPatients(df)
-
 
             ########### Vital signs ###########
             df = getHeartRate().dropna()
             self._putDataForPatients(df)
 
-
             df = getRespiratoryRate().dropna()
             self._putDataForPatients(df)
-
 
             df = getSystolicBloodPressure().dropna()
             self._putDataForPatients(df)
 
-
             df = getDiastolicBloodPressure().dropna()
             self._putDataForPatients(df)
-
 
             ########### Prognosis ###########
             df = getPreIcuLos().dropna()
             self._putDataForPatients(df)
 
-
             df = getHistoryACI()
             self._putDataForPatients(df)
-
 
             ########### Comorbidities ###########
             df = getHistoryAMI()
             self._putDataForPatients(df)
 
-
             df = getCHF()
             self._putDataForPatients(df)
-
 
             df = getLiverDisease()
             self._putDataForPatients(df)
 
-
             df = getPreExistingCKD()
             self._putDataForPatients(df)
-
 
             df = getMalignantCancer()
             self._putDataForPatients(df)
 
-
             df = getHypertension()
             self._putDataForPatients(df)
-
 
             df = getUTI()
             self._putDataForPatients(df)
 
-
             df = getChronicPulmonaryDisease()
             self._putDataForPatients(df)
-
 
             ########### Interventions ###########
             df = getMV()
             self._putDataForPatients(df)
 
-
             df = getNaHCO3()
             self._putDataForPatients(df)
-
 
             ########### Save file ###########
             Patients.toJsonFile(patientList, self.storagePath)
 
-
         pass
-    
+
     def _putDataForPatients(self, df):
         for patient in self.patientList:
             if "stay_id" in df.columns:
-                dfIndividualMeasures = df[df["stay_id"] == patient.stayId]
+                dfIndividualMeasures = df[df["stay_id"] == patient.stay_id]
             elif "hadm_id" in df.columns:
-                dfIndividualMeasures = df[df["hadm_id"] == patient.hadmId]
+                dfIndividualMeasures = df[df["hadm_id"] == patient.hadm_id]
             elif "subject_id" in df.columns:
-                dfIndividualMeasures = df[df["subject_id"] == patient.subjectId]
+                dfIndividualMeasures = df[df["subject_id"] == patient.subject_id]
             else:
                 print("DataFrame does not have 'hadm_id' or 'stay_id' column.")
                 return
@@ -478,9 +437,38 @@ class Patients:
 
     def savePatients(self, patients: Collection[Patient]):
         self.patientList = patients
-        
+
         Patients.toJsonFile(patients, self.storagePath)
 
+    def getMeasuresBetween(
+        self,
+        fromTime: pd.Timedelta,
+        toTime: pd.Timedelta,
+        how: str | Callable[[DataFrame], float] = "avg",
+    ):
+        """Get patient's status during specified period.
+
+        Args:
+            fromTime (Timedelta): start time compare to intime (icu admission)
+            toTime (Timedelta): end time compare to intime (icu admission)
+            how : {'first', 'last', 'avg', 'max', 'min', 'std'} | Callable[[DataFrame], float], default 'avg'
+                Which value to choose if multiple exist:
+
+                    - first: Use first recored value.
+                    - last: Use last recored value.
+                    - avg: Use average of values.
+                    - max: Use max value.
+                    - min: Use min value.
+                    - std: Use standard deviation of values
+                    - custom function that take dataframe(time, value) and return value
+
+        Returns:
+            DataFrame: one row with full status of patient
+        """
+
+        xLs = [x.getMeasuresBetween(fromTime, toTime, how) for x in self.patientList]
+
+        pd.concat(xLs)
 
     @staticmethod
     def toJsonFile(patients: Collection[Patient], file: str | Path):
