ea6fdd8a79d483869243df82b2d3b996b3e76e7c
bhtu <bhtu.work@gmail.com>
2024-08-08 21:06:47 +0700

    votting class

--- utils/class_voter.py
+++ utils/class_voter.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 
+class ModelWrapper:
+    def __init__(self, model, encoder):
+        self.model = model
+        self.encoder = encoder
+
+    def predict_proba(self, X):
+        X = self.encoder.transform(X)
+        return self.model.predict_proba(X)
+
+    def predict(self, X):
+        proba = self.predict_proba(X)
+        return [1 if p > 0.5 else 0 for p in proba]
+
 class Voter:
-    def __init__(self, models):
+    def __init__(self, models: list[ModelWrapper]):
         self.models = models
 
     def predict_proba(self, X, methodCalledForPredictProba="predict_proba"):
